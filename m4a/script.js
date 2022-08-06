61771a5d0b9c9e8a291241e6141acebef707eae7
unknown <meet.amogh.verma@gmail.com>
2022-08-06 20:35:55 +0530

    m4a

--- m4a/script.js
+++ m4a/script.js
@@ -28,19 +28,17 @@ assignment.
 WARNING!!! WARNING!!!
 
 */
-
+(function(window){
 // STEP 1:
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (var n in names) {
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -56,9 +54,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (names[n].charAt(0).toLowerCase()==='j') {
+	  window.byeSpeaker.speak(names[n])
   } else {
-    // helloSpeaker.xxxx
+	  window.helloSpeaker.speak(names[n])
   }
 }
+})(window);
